ede14fc0658ce9cf07c96e2c254e9c7a87630b39
Ademir Constantino <ademirconstantino@gmail.com>
2026-09-23 08:00:47 -0300

    footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,8 +1,7 @@
-
 import JsonReader from './JSonReader';
 import { useLang } from "./LangContext";
 
-import "../public/css/menu.css"; 
+import "../public/css/menu.css";
 import "../public/css/bootstrap.min.css";
 import "../public/css/theme-style.min.css";
 import "../public/css/custom-style.css";
@@ -10,47 +9,141 @@ import "../public/css/font-awesome.min.css";
 
 function Footer() {
 
-const { langSelected } = useLang();
+  const { langSelected } = useLang();
 
   return (
-    <div>
-    <div className="container" style={{ backgroundColor: '#F0F0F0', paddingTop: 20, width: "90%" }}>
+    <div className="site-footer">
+
+      <div
+        className="container footer-container"
+        style={{
+          backgroundColor: '#F0F0F0',
+          paddingTop: 20,
+          width: "90%"
+        }}
+      >
+
         <div className="row">
-            <div className="col-md-3 col">
+
+          <div className="col-md-3 col footer-contact">
+
             <div className="block contact-block">
-                <address>
+
+              <address>
+
                 <ul className="fa-ul">
-                    <li>
-                    <abbr title="Phone"><i className="fa fa-li fa-phone"></i></abbr>{JsonReader(langSelected, "footer.telephone_number")}</li>
-                    <li>
-                    <abbr title="Email"><i className="fa fa-li fa-envelope"></i></abbr>
-                    <a href="/cit-react/contact" style={{ textDecoration: 'none', color: 'black' }}>{JsonReader(langSelected, "footer.contact_us")}</a>
-                    </li>
-                    <li>
-                    <abbr title="Address"><i className="fa fa-li fa-home"></i></abbr>{JsonReader(langSelected, "footer.address")}</li>
+
+                  <li>
+                    <abbr title="Phone">
+                      <i className="fa fa-li fa-phone"></i>
+                    </abbr>
+                    {JsonReader(
+                      langSelected,
+                      "footer.telephone_number"
+                    )}
+                  </li>
+
+                  <li>
+                    <abbr title="Email">
+                      <i className="fa fa-li fa-envelope"></i>
+                    </abbr>
+
+                    <a
+                      href="/cit-react/contact"
+                      className="footer-contact-link"
+                    >
+                      {JsonReader(
+                        langSelected,
+                        "footer.contact_us"
+                      )}
+                    </a>
+                  </li>
+
+                  <li>
+                    <abbr title="Address">
+                      <i className="fa fa-li fa-home"></i>
+                    </abbr>
+                    {JsonReader(
+                      langSelected,
+                      "footer.address"
+                    )}
+                  </li>
+
                 </ul>
-                </address>
+
+              </address>
+
             </div>
-            </div>
+
+          </div>
+
         </div>
+
+
         <div className="row">
-            <div className="subfooter">
-            <div className="col-md-6">
-                <p>{JsonReader(langSelected, "footer.copyright_notice")}</p>
+
+          <div className="subfooter">
+
+            {/* COPYRIGHT */}
+            <div className="col-md-6 footer-copyright">
+
+              <p>
+                {JsonReader(
+                  langSelected,
+                  "footer.copyright_notice"
+                )}
+              </p>
+
             </div>
-            <div style={{ paddingRight: 20, textAlign: 'right'}}>
-                    <div>
-                        <a href="https://x.com/ItConstantino"><i className="fa fa-twitter" style={{paddingLeft: 5}}></i></a>
-                        <a href="https://www.facebook.com/profile.php?id=61581224173736"><i className="fa fa-facebook" style={{paddingLeft: 5}}></i></a>
-                        <a href="http://www.linkedin.com/in/ademir-constantino/"><i className="fa fa-linkedin" style={{paddingLeft: 5}}></i></a>
-                    </div>
-             </div>
-         </div>
+
+
+            {/* SOCIAL NETWORKS */}
+            <div
+              className="footer-social"
+              style={{
+                paddingRight: 20,
+                textAlign: 'right'
+              }}
+            >
+
+              <div>
+
+                <a href="https://x.com/ItConstantino">
+                  <i
+                    className="fa fa-twitter"
+                    style={{ paddingLeft: 5 }}
+                  ></i>
+                </a>
+
+                <a href="https://www.facebook.com/profile.php?id=61581224173736">
+                  <i
+                    className="fa fa-facebook"
+                    style={{ paddingLeft: 5 }}
+                  ></i>
+                </a>
+
+                <a href="http://www.linkedin.com/in/ademir-constantino/">
+                  <i
+                    className="fa fa-linkedin"
+                    style={{ paddingLeft: 5 }}
+                  ></i>
+                  
+                </a>
+
+              </div>
+
+            </div>
+
+          </div>
+
         </div>
-        </div>
-        <br/>
-        </div>
-    );
+
+      </div>
+
+      <br />
+
+    </div>
+  );
 }
 
 export default Footer;
